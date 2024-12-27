6f8999475405be34196c745a134f1f45f88dbefc
olv-leo <olv.leo@outlook.com>
2024-12-27 12:57:03 -0300

    feat: Add novo endpoint de produtos

--- testes.py
+++ testes.py
@@ -13,3 +13,13 @@ def test_ola_mundo_status_code():
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"Olá": "Mundo"}
+
+
+def test_listar_produtos_status_code():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+
+def test_tamanho_lista_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
